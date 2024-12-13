65c1878847007a41cbfacdd191e949b3dc33ff9b
Jyoti Palariya <palariyajyoti456@gmail.com>
2024-12-13 10:00:00 +0530

    Your commit message

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,7 @@
-MODEL_ID = "stabilityai/stable-diffusion-2-base"  # Text-to-image model
-DEVICE = "cpu"  
+# MODEL_ID = "stabilityai/stable-diffusion-2-base"  # Text-to-image model
+# DEVICE = "cpu"  
+
+
+# MODEL_ID = "stabilityai/stable-diffusion-2-base"  # Text-to-image model
+MODEL_ID = "CompVis/stable-diffusion-v1-4"  
+DEVICE = "cpu"
--- utils/text_to_image.py
+++ utils/text_to_image.py
@@ -1,18 +1,20 @@
 
+
 from diffusers import StableDiffusionPipeline
 from utils.config import MODEL_ID, DEVICE
+import torch
 import os
 
 def text_to_image(prompt, output_dir="outputs/generated_images"):
-   
-    pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID)
+    # Load the pre-trained model
+    pipe = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float32)
     pipe.to(DEVICE)
 
-
+    # Generate image
     print(f"Generating image for prompt: {prompt}")
-    image = pipe(prompt).images[0]
+    image = pipe(prompt, num_inference_steps=15).images[0]
 
-   
+    # Save the image
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, "generated_image.png")
     image.save(output_path)
